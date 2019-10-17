34446cfd0473a22ecdfa14fc7ecaf6bf58efa312
BeGear <BeGear@DESKTOP-BD4M8JC.station>
2019-10-17 11:06:40 +0200

    Aggiunti metodi di sottrazione, moltiplicazione e divisione

--- src/main/java/base/Calcolatrice.java
+++ src/main/java/base/Calcolatrice.java
@@ -5,5 +5,17 @@ public class Calcolatrice {
 	int somma(int a, int b) {
 		return a+b;
 	}
+	
+	int sottrazione(int a, int b) {
+		return a-b;
+	}
+	
+	int moltiplicazione(int a, int b) {
+		return a*b;
+	}
+	
+	int divisione(int a, int b) {
+		return a/b;
+	}
 
 }
